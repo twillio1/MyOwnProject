fd19fb0bf72fb43b8c26c93a1b063ec913b56aa2
Misra <subhajitmisra20@gmail.com>
2024-02-21 21:25:16 +0530

    New Commit_3

--- Test_3/src/main/java/com/Test/Test_3/App.java
+++ Test_3/src/main/java/com/Test/Test_3/App.java
@@ -14,6 +14,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("kabdqbdq");
        SessionFactory fac=HibernateUtil.getSession();
        
        Session session = fac.openSession();
